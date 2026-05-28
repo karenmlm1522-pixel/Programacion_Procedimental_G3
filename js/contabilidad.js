adac4450d2600cb99f1f5ccc2a51b6e2c657bc6f
Karen Martinez <karenmlm1522@gmail.com>
2026-05-28 17:44:40 -0500

    get_user_information

---
+++ js/contabilidad.js
@@ -0,0 +1,5 @@
+let ventas = document.getElementById("ventas").value;
+let fechas = document.getElementById("fechas").value;
+let ingresos = document.getElementById("ingresos").value;
+let egresos = document.getElementById("egresos").value;
+let valor_total = document.getElementById("valor_total").value;
---
+++ js/usuario.js
@@ -0,0 +1,8 @@
+let nombre = document.getElementById("nombre").value;
+let apellido = document.getElementById("apellido").value;
+let numero_documento = document.getElementById("numero_documento").value;
+let telefono = document.getElementById("telefono").value;
+let email = document.getElementById("email").value;
+let Cargo = document.getElementById("Cargo").value;
+let contraseña = document.getElementById("contraseña").value;
+
